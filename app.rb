3608c1a3a7b9820859899df592b537b638c5dbce
Jihin Ip <jim_ip20@hotmail.com>
2018-04-05 15:05:07 +0100

    Adding 2 players, passing rspec tests

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@ require 'sinatra'
 
 class Battle < Sinatra::Base
   get '/' do
-    "Hello Battle!"
+    erb(:index)
+  end
+
+  post '/names' do
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    erb(:play)
   end
 end
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,1 +1,20 @@
 require './app.rb'
+require 'capybara/rspec'
+
+Capybara.app = Battle
+
+feature "Testing homepage" do
+
+
+end
+
+feature "Entering player names" do
+  scenario "enter player names into the form" do
+    visit("/")
+    fill_in :player1, with: "Jihin"
+    fill_in :player2, with: "Louis"
+
+    click_button "Submit"
+    expect(page).to have_content "Player 1: Jihin! Player 2: Louis!"
+  end
+end
