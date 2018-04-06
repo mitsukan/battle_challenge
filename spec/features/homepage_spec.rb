34f1c9ede8dcba13f9164060fda45475e7f076ee
Jihin Ip <jim_ip20@hotmail.com>
2018-04-06 12:25:42 +0100

    Refactoring homepage_spec

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -7,7 +7,7 @@ Capybara.app = Battle
 feature "Entering player names" do
   scenario "enter player names into the form" do
     sign_in_and_play
-    expect(page).to have_content "Player 1: Testplayer1! Player 2: Testplayer2!"
+    expect(page).to have_content "Player 1: Testplayer1!", "Player 2: Testplayer2!"
   end
 end
 
